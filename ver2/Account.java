8d9049953a125a83eaddf2aff1792e5357e06c74
zoeMac <mcswine.zoe@gmail.com>
2025-09-02 16:42:03 -0400

    commit title

--- ver2/Account.java
+++ ver2/Account.java
@@ -6,11 +6,7 @@ public class Account {
 		private double balance;
 		private String name;
 		
-		// Constructor
-		public Account(double balance) {
-			this("Unknown", balance);
-
-		}
+		
 
 		
 		
